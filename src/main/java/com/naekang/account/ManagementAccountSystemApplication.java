ee4fb3de5ac160e1fca376166657d200b40a21f0
naekang <rlawlsgh6306@gmail.com>
2022-09-12 16:47:52 +0900

    Initial Settings

---
+++ src/main/java/com/naekang/account/ManagementAccountSystemApplication.java
@@ -0,0 +1,13 @@
+package com.naekang.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAccountSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementAccountSystemApplication.class, args);
+    }
+
+}
